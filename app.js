cb24daee5f6c269881dcaab153fbf387db68e602
vicky <victoriabarbero88@gmail.com>
2020-08-25 16:48:40 +0200

    cors headers control

--- app.js
+++ app.js
@@ -33,11 +33,15 @@ app.use(
     origin: [process.env.PUBLIC_DOMAIN, 'https://face-for-paws.web.app'], 
   })
 );
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://face-for-paws.web.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Max-Age", "1800");
+	res.setHeader("Access-Control-Allow-Headers", "content-type");
   next();
 });
 
